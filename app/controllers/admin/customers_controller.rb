ec794fa79b145e603077a91e57795c23481c19cd
EC2 Default User <ec2-user@ip-172-31-32-93.ec2.internal>
2022-02-15 09:17:47 +0000

    修正

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,9 +1,16 @@
 class Admin::CustomersController < ApplicationController
+  before_action :set_customer, only: [:show]
+
   def index
     @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
+  end
+
+  private
+  def set_customer
     @customer = Customer.find(params[:id])
   end
 end
