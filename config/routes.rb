9b8d1f54e65fdae442400a248661c63a6385aa3f
tia54uba <93515097+tia54uba@users.noreply.github.com>
2022-02-17 05:06:47 +0000

    add products views controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,27 @@
 Rails.application.routes.draw do
-  root 'public/products#top'
-  get 'about', to: 'public/products#about'
+  root 'public/homes#top'
+  get 'about', to: 'public/homes#about'
 
-   # 顧客用
-  # URL /customers/sign_in ...
-  devise_for :customers, skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-  }
+  # 顧客用
+    # URL /customers/sign_in ...
+    devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+    }
 
   # 管理者用
-  # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-  sessions: "admin/sessions"
-  }
+    # URL /admin/sign_in ...
+    devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+    }
 
   #会員側
   scope module: 'public' do
-    resources :customers, only: [:show, :edit, :update]
     get 'customers/products', to: 'products#index', as: 'products'
-    get 'customers/products', to: 'products#show', as: 'product'
+    get 'customers/products/:id', to: 'products#show', as: 'product'
     resources :customers, only: [:show, :edit, :update]
-    get '/customers/id/withdraw' => 'customers#withdraw', as: 'withdraw_customer' #大会画面への遷移
-    patch '/customers/:id/withdraw' => 'customers#switch', as: 'withdraw_switch_customer' #会員ステータスの切り替え
+    get '/customers/:id/withdraw' => 'customers#withdraw', as: 'withdraw_customer' #退会画面への遷移
+    patch '/customers/:id/withdraw' => 'customers#switch', as: 'withdraw_switch_customer' #会員ステータスの切替
   end
 
   #管理者側
