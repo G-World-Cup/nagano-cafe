f81f902011d11a692305b902b1b0bc8a6238d5ca
Miyawaki <tetsu.miyawaki@gmail.com>
2022-02-17 05:00:04 +0000

    Edit Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
   root 'public/homes#top'
   get 'about', to: 'public/homes#about'
-  
+
   # 顧客用
     # URL /customers/sign_in ...
     devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
     }
-    
+
   # 管理者用
     # URL /admin/sign_in ...
     devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
     }
-    
+
   #会員側
   scope module: 'public' do
     get 'customers/products', to: 'products#index', as: 'products'
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
     get '/customers/:id/withdraw' => 'customers#withdraw', as: 'withdraw_customer' #退会画面への遷移
     patch '/customers/:id/withdraw' => 'customers#switch', as: 'withdraw_switch_customer' #会員ステータスの切替
   end
-  
+
   #管理者側
   namespace :admin do
     resources :customers, only: [:index, :show, :edit, :update]
