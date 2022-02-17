506c25d887286e75f2bc638172bc69f91a816db5
Miyawaki <tetsu.miyawaki@gmail.com>
2022-02-17 04:30:15 +0000

    Add Customer-Edit-Withdraw

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,33 @@
 Rails.application.routes.draw do
-  root 'public/products#top'
-  get 'about', to: 'public/products#about'
-
+  root 'public/homes#top'
+  get 'about', to: 'public/homes#about'
+  
+  # 顧客用
+    # URL /customers/sign_in ...
+    devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+    }
+    
+  # 管理者用
+    # URL /admin/sign_in ...
+    devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+    }
+    
   #会員側
   scope module: 'public' do
+    get 'customers/products', to: 'products#index', as: 'products'
+    get 'customers/products/:id', to: 'products#show', as: 'product'
     resources :customers, only: [:show, :edit, :update]
-    get 'customers/products', to: 'products#index'
-    get 'customers/products', to: 'products#show'
+    get '/customers/:id/withdraw' => 'customers#withdraw', as: 'withdraw_customer' #退会画面への遷移
+    patch '/customers/:id/withdraw' => 'customers#switch', as: 'withdraw_switch_customer' #会員ステータスの切替
   end
-
+  
   #管理者側
   namespace :admin do
     resources :customers, only: [:index, :show, :edit, :update]
     get 'top', to: 'homes#top'
     resources :products, only: [:new, :index, :show, :create]
   end
-
-
-  # 顧客用
-  # URL /customers/sign_in ...
-  devise_for :customers, skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-  }
-
-
-  # 管理者用
-  # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-  sessions: "admin/sessions"
-  }
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
