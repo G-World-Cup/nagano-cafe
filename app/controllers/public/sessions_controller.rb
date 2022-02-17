81a31f3f5bbe7308d4bfddb94537ce6333d3cdad
Alexander Kun <ec2-user@ip-172-31-44-120.ec2.internal>
2022-02-17 04:45:23 +0000

    Add sessions controller

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
   # GET /resource/sign_in
   # def new
   #   super
@@ -18,6 +19,19 @@ class Public::SessionsController < Devise::SessionsController
   # end
 
   # protected
+  protected
+    # 退会しているかを判断するメソッド
+  def customer_state
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@customer
+    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @customer.valid_password?(params[:customer][:password])
+      ## 【処理内容3】
+      redirect_to new_customer_registration_path
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
