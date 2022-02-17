0f56570a836b91d3cedb063190a1d81e6c24dd08
EC2 Default User <ec2-user@ip-172-31-32-93.ec2.internal>
2022-02-17 04:29:36 +0000

    Add admin edit update

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,11 +1,19 @@
 class Admin::CustomersController < ApplicationController
-  before_action :set_customer, only: [:show]
+  before_action :set_customer, only: [:show, :edit, :update]
 
   def index
     @customers = Customer.all
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
   end
 
   private
