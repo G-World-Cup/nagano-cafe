51e3cb10f04990c5972574130b9e6558e171d792
Miyawaki <tetsu.miyawaki@gmail.com>
2022-02-20 06:16:22 +0000

    Test

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -20,4 +20,10 @@ class Admin::CustomersController < ApplicationController
   def set_customer
     @customer = Customer.find(params[:id])
   end
+
+  def customer_params
+  	params.require(:customer).permit(:is_deleted, :last_name, :first_name, :last_name_kana, :first_name_kana,
+  	                                   :phone_number, :email, :password, :postcode, :address)
+  end
+
 end
