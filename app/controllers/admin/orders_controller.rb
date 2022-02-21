655c8158091464880d7da23d42c45e03c987ce95
tia54uba <93515097+tia54uba@users.noreply.github.com>
2022-02-21 07:09:05 +0000

    add orders show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,6 +7,10 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order_details = @order.order_details
+    @total_price = 0
+    @order_details.each do |order_detail|
+    @total_price += order_detail.subtotal_price
+    end
   end
 
   def update
