f571e01303139c364b2f913b0c3afdc1b640b88c
Alexander Kun <ec2-user@ip-172-31-44-120.ec2.internal>
2022-02-23 10:03:53 +0000

    fix admin products edit page, edit seed file for presentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 
 Admin.create!(
-    email: 'abc@123.com',
+    email: 'admin@naganocake.com',
     password: '123456',
     password_confirmation: '123456'
   )
@@ -18,10 +18,49 @@ Customer.create!(
   first_name: "太郎",
   last_name_kana: "ヤマダ",
   first_name_kana: "タロウ",
-  postcode: "123456789",
-  address: "大阪",
-  phone_number: "987654321",
-  email: "e@e",
+  postcode: "44329606",
+  address: "大阪二十日町",
+  phone_number: "030231973",
+  email: "yamada@yafoo.com",
+  password: "password",
+  password_confirmation: "password"
+  )
+
+  Customer.create!(
+  last_name: "山本",
+  first_name: "二郎",
+  last_name_kana: "ヤマモト",
+  first_name_kana: "ジロウ",
+  postcode: "04024876",
+  address: "大阪十九町",
+  phone_number: "0582863",
+  email: "yamamoto@qmail.com",
+  password: "password",
+  password_confirmation: "password"
+  )
+
+  Customer.create!(
+  last_name: "山口",
+  first_name: "三太郎",
+  last_name_kana: "ヤマグチ",
+  first_name_kana: "サンタロウ",
+  postcode: "00354967",
+  address: "大阪十八町",
+  phone_number: "080394656",
+  email: "yamaguchi@headbook.com",
+  password: "password",
+  password_confirmation: "password"
+  )
+
+  Customer.create!(
+  last_name: "山崎",
+  first_name: "四太郎",
+  last_name_kana: "ヤマザキ",
+  first_name_kana: "ヨンタロウ",
+  postcode: "034283762",
+  address: "大阪十七町",
+  phone_number: "070674285",
+  email: "yamazaki@zwitter.com",
   password: "password",
   password_confirmation: "password"
   )
@@ -31,18 +70,53 @@ Genre.create!(name: 'プリン')
 Genre.create!(name: 'タルト')
 Genre.create!(name: 'クッキー')
 Genre.create!(name: '和菓子')
-Genre.create!(name: 'ドーナツ')
 
-  5.times do |i|
-    cake = Product.new(
-      name: 'Cheese cake',
-      introduction: 'Yummy cheese cake from Nagano!',
-      genre_id: 0,
-      image_id: '',
-      price: 450,
-      is_active: true,
-      genre: Genre.find(1)
-      )
+Product.create!(
+    name: 'チーズケーキ',
+    introduction: '令和２年に長野県でのチーズケーキコンテストで一位をとったチーズケーキでございます。',
+    genre_id: 1,
+    image_id: '',
+    price: 500,
+    is_active: true,
+    genre: Genre.find(1)
+  )
 
-    cake.save
-  end
+  Product.create!(
+    name: 'キラキラカップケーキ',
+    introduction: 'とてもふわふわで、お子様に人気一番のカップケーキでございます。',
+    genre_id: 1,
+    image_id: '',
+    price: 400,
+    is_active: true,
+    genre: Genre.find(1)
+  )
+
+  Product.create!(
+    name: 'タルトショコラ',
+    introduction: 'チョコが好きなお方にはオススメです。',
+    genre_id: 3,
+    image_id: '',
+    price: 800,
+    is_active: true,
+    genre: Genre.find(3)
+  )
+
+  Product.create!(
+    name: '蕎麦茶プリン',
+    introduction: '蕎麦茶プリンは珍しいですが、ぜひお試しください。',
+    genre_id: 2,
+    image_id: '',
+    price: 600,
+    is_active: true,
+    genre: Genre.find(2)
+  )
+
+  Product.create!(
+    name: '苺大福',
+    introduction: '季節のいちごを使用して、お子様からお年寄りまで、誰でも大好きな苺大福でございます。',
+    genre_id: 5,
+    image_id: '',
+    price: 600,
+    is_active: true,
+    genre: Genre.find(5)
+  )
