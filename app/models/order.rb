662e350887eaf69d6d858dab9955230e80b06154
Alexander Kun <ec2-user@ip-172-31-44-120.ec2.internal>
2022-02-23 04:59:58 +0000

    resolve order model conflict

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,13 +6,9 @@ class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum order_status: { wait_pay: 0, check_pay: 1, in_production: 2, preparing_ship: 3, shipped: 4 }
 
-  validates :customer_postcode, presence: true
-  validates :customer_address, presence: true
-  validates :customer_name, presence: true
-  validates :payment_method, presence: true
-  validates :total_price, presence: true
-
   def total_count
     self.order_details.all.sum(:count)
   end
+
+
 end
