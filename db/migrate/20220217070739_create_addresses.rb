b65bb3f9d5b9fb9845824e716e8ae4b8e1c1a7a6
Alexander Kun <ec2-user@ip-172-31-44-120.ec2.internal>
2022-02-23 04:42:53 +0000

    Fix order controllers errors

--- db/migrate/20220217070739_create_addresses.rb
+++ db/migrate/20220217070739_create_addresses.rb
@@ -1,10 +1,10 @@
 class CreateAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :addresses do |t|
-      t.string :name
       t.integer :customer_id
-      t.string :postal_code
-      t.string :shipping_address
+      t.string :customer_name
+      t.string :customer_address
+      t.string :customer_postcode
 
       t.timestamps
     end
